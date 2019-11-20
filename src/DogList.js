b915ae0ad185d89f6d3e8a7ce66ac103ad28e74c
Sam <super8989@gmail.com>
2019-11-19 23:02:29 -0500

    added in bootstrap class to each dog

--- src/DogList.js
+++ src/DogList.js
@@ -9,7 +9,7 @@ class DogList extends Component {
 				<div className='container'>
 					<div className='row'>
 						{this.props.dogs.map(d => (
-							<div className='Dog col-4 text-center' key={d.name}>
+							<div className='Dog col-lg-4 text-center' key={d.name}>
 								<img src={d.src} alt={d.name} />
 								<h3>{d.name}</h3>
 							</div>
