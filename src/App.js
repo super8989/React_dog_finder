2094a2f3df3c62a122b5a8ffe459022625242f74
Sam <super8989@gmail.com>
2019-11-19 22:46:21 -0500

    defaultProps for dog data

--- src/App.js
+++ src/App.js
@@ -1,90 +1,49 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-	return (
-		<div className='App'>
-			<nav class='navbar navbar-expand-lg navbar-light bg-light'>
-				<a class='navbar-brand' href='#'>
-					Navbar
-				</a>
-				<button
-					class='navbar-toggler'
-					type='button'
-					data-toggle='collapse'
-					data-target='#navbarSupportedContent'
-					aria-controls='navbarSupportedContent'
-					aria-expanded='false'
-					aria-label='Toggle navigation'
-				>
-					<span class='navbar-toggler-icon'></span>
-				</button>
+class App extends Component {
+	static defaultProps = {
+		dogs: [
+			{
+				name: "Whiskey",
+				age: 5,
+				src: "whiskey",
+				facts: [
+					"Whiskey loves eating popcorn.",
+					"Whiskey is a terrible guard dog.",
+					"Whiskey wants to cuddle with you!"
+				]
+			},
+			{
+				name: "Hazel",
+				age: 3,
+				src: "hazel",
+				facts: [
+					"Hazel has so much energy!",
+					"Hazel is highly intelligent",
+					"Hazel loves people more than dogs"
+				]
+			},
+			{
+				name: "Tubby",
+				age: 4,
+				src: "tubby",
+				facts: [
+					"Tubby is a fatty",
+					"Tubby does not like exercise",
+					"Tubby loves eating food"
+				]
+			}
+		]
+	};
 
-				<div class='collapse navbar-collapse' id='navbarSupportedContent'>
-					<ul class='navbar-nav mr-auto'>
-						<li class='nav-item active'>
-							<a class='nav-link' href='#'>
-								Home <span class='sr-only'>(current)</span>
-							</a>
-						</li>
-						<li class='nav-item'>
-							<a class='nav-link' href='#'>
-								Link
-							</a>
-						</li>
-						<li class='nav-item dropdown'>
-							<a
-								class='nav-link dropdown-toggle'
-								href='#'
-								id='navbarDropdown'
-								role='button'
-								data-toggle='dropdown'
-								aria-haspopup='true'
-								aria-expanded='false'
-							>
-								Dropdown
-							</a>
-							<div class='dropdown-menu' aria-labelledby='navbarDropdown'>
-								<a class='dropdown-item' href='#'>
-									Action
-								</a>
-								<a class='dropdown-item' href='#'>
-									Another action
-								</a>
-								<div class='dropdown-divider'></div>
-								<a class='dropdown-item' href='#'>
-									Something else here
-								</a>
-							</div>
-						</li>
-						<li class='nav-item'>
-							<a
-								class='nav-link disabled'
-								href='#'
-								tabindex='-1'
-								aria-disabled='true'
-							>
-								Disabled
-							</a>
-						</li>
-					</ul>
-					<form class='form-inline my-2 my-lg-0'>
-						<input
-							class='form-control mr-sm-2'
-							type='search'
-							placeholder='Search'
-							aria-label='Search'
-						/>
-						<button class='btn btn-outline-success my-2 my-sm-0' type='submit'>
-							Search
-						</button>
-					</form>
-				</div>
-			</nav>
-			<h1 className='display-1'>Dog App</h1>
-		</div>
-	);
+	render() {
+		return (
+			<div className='App'>
+				<h1 className='display-1'>Dog App</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
