c8f3d810b740eebb36605dea5bbfa1ed6fd28857
Sam <super8989@gmail.com>
2019-11-19 23:19:42 -0500

    displaying the information in DogDetails.js

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -2,7 +2,18 @@ import React, { Component } from "react";
 
 class DogDetails extends Component {
 	render() {
-		return <h1>Dog Details</h1>;
+		let { dog } = this.props;
+
+		return (
+			<div className='DogDetails row'>
+				<div className='col-11 col-lg-5'>
+					<div className='DogDetails-card card'>
+						<img className='card-img-top' src={dog.src} alt={dog.name} />
+					</div>
+				</div>
+				<h1>{this.props.dog.name}</h1>
+			</div>
+		);
 	}
 }
 
