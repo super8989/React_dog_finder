a01711bced198d78b05e7cbc12655a45a47d3759
Sam <super8989@gmail.com>
2019-11-20 22:37:18 -0500

    created a Route.js to refactor and move the Routes from App.js to Routes.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
-import DogList from "./DogList";
-import DogDetails from "./DogDetails";
+
 import Navbar from "./Navbar";
-import { Switch, Route } from "react-router-dom";
+
 import whiskey from "./images/whiskey.jpg";
 import hazel from "./images/hazel.jpg";
 import tubby from "./images/tubby.jpg";
@@ -45,25 +44,9 @@ class App extends Component {
 	};
 
 	render() {
-		const getDog = props => {
-			let name = props.match.params.name;
-			let currentDog = this.props.dogs.find(
-				dog => dog.name.toLowerCase() === name.toLowerCase()
-			);
-			return <DogDetails {...props} dog={currentDog} />;
-		};
-
 		return (
 			<div>
 				<Navbar dogs={this.props.dogs} />
-				<Switch>
-					<Route
-						exact
-						path='/dogs'
-						render={() => <DogList dogs={this.props.dogs} />}
-					/>
-					<Route exact path='/dogs/:name' render={getDog} />
-				</Switch>
 			</div>
 		);
 	}
