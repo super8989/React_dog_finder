33032f5a639b4dc54dcd0f2f464152190c86ad4e
Sam <super8989@gmail.com>
2019-11-20 22:07:11 -0500

    displaying dog info in DogDetails.js

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./DogDetails.css";
+import { Link } from "react-router-dom";
 
 class DogDetails extends Component {
 	render() {
@@ -11,6 +11,22 @@ class DogDetails extends Component {
 					<div className='col-11 col-lg-5'>
 						<div className='DogDetails-card card'>
 							<img className='card-img-top' src={dog.src} alt={dog.name} />
+							<div className='card-body'>
+								<h2 className='card-title'>{dog.name}</h2>
+								<h4 className='card-subtitle text-muted'>
+									{dog.age} years old
+								</h4>
+							</div>
+							<ul className='list-group list-group-flush'>
+								{dog.facts.map((fact, i) => (
+									<li className='list-group-item' key={i}>
+										{fact}
+									</li>
+								))}
+							</ul>
+							<div className='card-body'>
+								<Link to='/dogs'>Go Back</Link>
+							</div>
 						</div>
 					</div>
 				</div>
