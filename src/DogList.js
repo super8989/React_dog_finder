d203fb20d3c7291687cf2eb1b13452418af7c124
Sam <super8989@gmail.com>
2019-11-21 22:48:41 -0500

    added margin on DogList

--- src/DogList.js
+++ src/DogList.js
@@ -6,7 +6,7 @@ class DogList extends Component {
 	render() {
 		return (
 			<div className='DogList'>
-				<h1 className='display-1 text-center'>Dog List</h1>
+				<h1 className='display-1 text-center my-5'>Dog List</h1>
 				<div className='row'>
 					{this.props.dogs.map(d => (
 						<div className='Dog col-lg-4 text-center' key={d.name}>
